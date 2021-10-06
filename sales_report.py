22b125d1e3cc9dddbc32ee31f8fdad541620bea8
Vallie Tracy <trac0055@umn.edu>
2021-10-06 07:26:29 -0500

    working thru adding comments

--- sales_report.py
+++ sales_report.py
@@ -1,12 +1,19 @@
 """Generate sales report showing total melons each salesperson sold."""
 
-
+# Creating global variables
+# Empty lists which will store names of salespeople and another list which will store # of melons sold
+# Might want to make a dictionary instead of two separate lists where person is key and value is melons sold
 salespeople = []
 melons_sold = []
 
+# Opening the file and storing it in a variable called 'f'
 f = open('sales-report.txt')
+
+# Loop thru every line in the file 'f'
 for line in f:
+    # Strip each line of white space
     line = line.rstrip()
+    # Split the line at '|' character and store it in a new variable 'entries'
     entries = line.split('|')
 
     salesperson = entries[0]
